985665f168efde727422f722de881e5b79014c51
Owen Wang <wangfowen@gmail.com>
2014-01-25 14:40:36 +0100

    basic cms

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tedxuw::Application.config.secret_key_base = 'c93cd68991fa705775a5495d79eea32b8a1a7ffad82715ed8ca95429f8c83430b88f5bf5b8c5f8718c3a3f1cb6e138ad142ed374aebf76ff963a8ad66dd7cc2a'
